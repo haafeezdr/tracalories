83a6fef6c1ae7012e3811b636177e158269b00d5
haafeez <haafeezdaiyab@gmail.com>
2022-07-17 12:22:34 -1200

    commit

--- app.js
+++ app.js
@@ -23,13 +23,30 @@
     totalCalories: 0
   }
 
-
-
-
-  // public methods
+ // public methods
   return {
     getItems: function(){
       return data.items;
+    },
+    addItem: function(name, calories){
+      let ID;
+      // create id
+      if(data.items.length > 0){
+        ID = data.items[data.items.length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // calories to number
+      calories = parseInt(calories);
+
+      // create new item
+      newItem = new Item(ID, name, calories);
+
+      // add to items array
+      data.items.push(newItem);
+
+      return newItem;
     },
     logData: function(){
         return data;
@@ -42,7 +59,10 @@
  // ui controller
  const UICtrl = (function(){
   const UISelectors = {
-    itemlist: '#item-list'
+    itemlist: '#item-list',
+    addBtn: '.add-btn',
+    itemNameInput: '#item-name',
+    itemCaloriesInput: '#item-calories'
   }
 
     
@@ -62,6 +82,15 @@
 
           // insert list items
           document.querySelector(UISelectors.itemlist).innerHTML = html;
+      },
+      getItemInput: function(){
+        return {
+          name:document.querySelector(UISelectors.itemNameInput).value,
+          calories:document.querySelector(UISelectors.itemCaloriesInput).value
+        }
+      }, 
+      getSelectors: function(){
+        return UISelectors;
       }
     }
  })();
@@ -71,19 +100,43 @@
 
  // app controller
  const App = (function(ItemCtrl, UICtrl){
+    // load event listeners
+    const loadEventListeners = function(){
+       // get ui selectors
+       const UISelectors = UICtrl.getSelectors();
+
+       // add item event
+       document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+    }
+
+    // add item submit
+    const itemAddSubmit = function(e){
+      // get form input from ui controller
+      const input = UICtrl.getItemInput();
+
+      // check for name and calories input
+      if(input.name !== '' && input.calories !== ''){
+        // add item
+        const newItem = ItemCtrl.addItem(input.name, input.calories);
+      }
+      e.preventDefault();
+    }
    
    // public methods
-   return {
+   return { 
     init: function(){
       // fetch items from data structure
         const items = ItemCtrl.getItems();
       
       // populate list with items
       UICtrl.populateItemList(items); 
+
+      // load event listeners
+      loadEventListeners();
     }
    }
 
  })(ItemCtrl, UICtrl);
   
  // initialize app
- App.init();
+ App.init(); 
